e0a7c8b559848a7a579a15d138669234a739db38
Shubham Sharma <shubham.sharma@nexxbase.com>
2026-01-10 19:16:13 +0530

    Update dependency array in useCallback for addNode functionality in PipelineUI component

--- frontend/src/ui.js
+++ frontend/src/ui.js
@@ -114,7 +114,7 @@ export const PipelineUI = () => {
             addNode(newNode);
           }
         },
-        [reactFlowInstance]
+        [reactFlowInstance, getNodeID, addNode]
     );
 
     const onDragOver = useCallback((event) => {
